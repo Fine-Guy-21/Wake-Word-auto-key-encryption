2be619e930ebed2cb99510efb972863d10c92a41
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2024-04-15 13:49:04 +0300

    Asp net file creation

---
+++ Word_Auto_Key_Encryption Algorithm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Word_Auto_Key_Encryption_Algorithm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
