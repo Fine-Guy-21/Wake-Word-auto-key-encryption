064cbba27f494d6a2d4bfde221a97c0fbfbb3d75
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2024-04-15 23:12:36 +0300

    Wake Controller added

---
+++ Word_Auto_Key_Encryption Algorithm/Controllers/WakeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Word_Auto_Key_Encryption_Algorithm.Controllers
+{
+    public class WakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Encrypt()
+        {
+
+            return View();
+        }
+    }
+}
